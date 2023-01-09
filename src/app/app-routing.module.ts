9eb840a9cfc52212b9d2b216c0a10f4ae37789c3
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2023-01-09 08:24:04 -0300

    build SE ELIMINARON LOS COMPO que no se usan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EnviadorAltagamaComponent } from './pages/enviador-altagama/enviador-altagama.component';
-import { EnviadorAtcComponent } from './pages/enviador-atc/enviador-atc.component';
-import { EnviadorComercialComponent } from './pages/enviador-comercial/enviador-comercial.component';
-import { EnviadorVentasComponent } from './pages/enviador-ventas/enviador-ventas.component';
 import { EnviadorComponent } from './pages/enviador/enviador.component';
-import { HomeComponent } from './pages/home/home.component';
-
 
 const routes: Routes = [
   {
@@ -15,23 +10,7 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'home', component: HomeComponent,
-    //canActivate: [AuthGuard]
-  },
-  {
     path: 'enviador', component: EnviadorComponent,
-    //canActivate: [AuthGuard]
-  },
-  {
-    path: 'enviadorAtc', component: EnviadorAtcComponent,
-    //canActivate: [AuthGuard]
-  },
-  {
-    path: 'enviadorComercial', component: EnviadorComercialComponent,
-    //canActivate: [AuthGuard]
-  },
-  {
-    path: 'enviadorVentas', component: EnviadorVentasComponent,
     //canActivate: [AuthGuard]
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EnviadorComponent } from './pages/enviador/enviador.component';
-import { EnviadorAtcComponent } from './pages/enviador-atc/enviador-atc.component';
-import { EnviadorComercialComponent } from './pages/enviador-comercial/enviador-comercial.component';
-import { EnviadorVentasComponent } from './pages/enviador-ventas/enviador-ventas.component';
 import { EnviadorAltagamaComponent } from './pages/enviador-altagama/enviador-altagama.component';
-import { HomeComponent } from './pages/home/home.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     EnviadorComponent,
-    EnviadorAtcComponent,
-    EnviadorComercialComponent,
-    EnviadorVentasComponent,
     EnviadorAltagamaComponent,
-    HomeComponent
   ],
   imports: [
     CommonModule, // Se importa para el toastr
